694041368b9d61abdcb0d21600fae8a304d1c074
p-98 <34243013+p-98@users.noreply.github.com>
2021-10-23 17:43:09 +0200

    improved HighlightedStates
    - secondary color support
    - refactored css to better imitate mdc

--- client/src/components/highlightStates/highlightStates.tsx
+++ client/src/components/highlightStates/highlightStates.tsx
@@ -4,13 +4,26 @@ import cn from "classnames";
 // local
 import styles from "./highlightStates.module.css";
 
+export const activatedClassName = cn(
+    styles["highlighted-states"],
+    styles["highlighted-states--activated"]
+);
+export const selectedClassName = cn(
+    styles["highlighted-states"],
+    styles["highlighted-states--selected"]
+);
+export const nonInteractiveClassName =
+    styles["highlighted-states--non-interactive"];
+
 interface IHighlightStatesPrps {
+    /** use secondary instead of primary theme color */
+    secondary?: boolean;
     activated?: boolean;
     selected?: boolean;
     children?: React.ReactElement<React.HTMLAttributes<HTMLElement>>;
 }
 export const HightlightStates = React.memo<IHighlightStatesPrps>(
-    ({ activated, selected, children }: IHighlightStatesPrps) => {
+    ({ activated, selected, secondary, children }: IHighlightStatesPrps) => {
         if (!React.isValidElement(children))
             throw new TypeError("children is not a valid react element!");
 
@@ -18,6 +31,7 @@ export const HightlightStates = React.memo<IHighlightStatesPrps>(
             className: cn(
                 children.props.className,
                 styles["highlighted-states"],
+                secondary && styles["highlighted-states--secondary"],
                 activated && styles["highlighted-states--activated"],
                 selected && styles["highlighted-states--selected"]
             ),
