88288434ed7cb968517867a62c93897d9085fea8
p-98 <34243013+p-98@users.noreply.github.com>
2021-10-23 10:49:55 +0200

    drawer highlight current item

--- client/src/scenes/_app/components/navigation.tsx
+++ client/src/scenes/_app/components/navigation.tsx
@@ -1,5 +1,6 @@
 import { List, SimpleListItem } from "@rmwc/list";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 // list imports
 import "@material/list/dist/mdc.list.css";
@@ -8,17 +9,25 @@ import "@rmwc/icon/icon.css";
 
 import routingObj from "Utility/routing";
 
-export const Navigation: React.FC = () => (
-    <List>
-        {routingObj.map((generator) => {
-            const { label, href, icon } = generator();
+export const Navigation: React.FC = () => {
+    const { pathname } = useRouter();
 
-            return (
-                // prefetching is disabled because it would load literally every page
-                <Link href={href} passHref prefetch={false} key={label}>
-                    <SimpleListItem graphic={icon} text={label} />
-                </Link>
-            );
-        })}
-    </List>
-);
+    return (
+        <List>
+            {routingObj.map((generator) => {
+                const { label, href, icon } = generator();
+
+                return (
+                    // prefetching is disabled because it would load literally every page
+                    <Link href={href} passHref prefetch={false} key={label}>
+                        <SimpleListItem
+                            graphic={icon}
+                            text={label}
+                            activated={pathname.startsWith(href)}
+                        />
+                    </Link>
+                );
+            })}
+        </List>
+    );
+};
