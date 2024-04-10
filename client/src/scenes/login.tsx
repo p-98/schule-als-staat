831190239b11e3b18bac5eb9ade3d79599aef458
p-98 <34243013+p-98@users.noreply.github.com>
2024-04-10 14:45:38 +0200

    feat(client): redirect after login

--- client/src/scenes/login.tsx
+++ client/src/scenes/login.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import { GridCell } from "Components/material/grid";
 import { Card } from "Components/material/card";
 
@@ -5,20 +6,26 @@ import { Card } from "Components/material/card";
 import { GridPage } from "Components/page/page";
 import { Login as LoginComponent } from "Components/login/login";
 import { DrawerAppBarHandle } from "Components/dynamicAppBar/presets";
+import { dispatch } from "Utility/misc";
 
-export const Login: React.FC = () => (
-    <GridPage>
-        <DrawerAppBarHandle title="Login" />
-        <GridCell desktop={4} tablet={2} phone={0} />
-        <GridCell span={4}>
-            <Card>
-                <LoginComponent
-                    onSuccess={() => console.log("logged in!")}
-                    title="Anmelden"
-                    confirmButton={{ label: "Anmelden" }}
-                    userBanner={{ label: "Anmelden als" }}
-                />
-            </Card>
-        </GridCell>
-    </GridPage>
-);
+export const Login: React.FC = () => {
+    const router = useRouter();
+    return (
+        <GridPage>
+            <DrawerAppBarHandle title="Login" />
+            <GridCell desktop={4} tablet={2} phone={0} />
+            <GridCell span={4}>
+                <Card>
+                    <LoginComponent
+                        onSuccess={() =>
+                            dispatch(router.push("/bankAccountInfo"))
+                        }
+                        title="Anmelden"
+                        confirmButton={{ label: "Anmelden" }}
+                        userBanner={{ label: "Anmelden als" }}
+                    />
+                </Card>
+            </GridCell>
+        </GridPage>
+    );
+};
